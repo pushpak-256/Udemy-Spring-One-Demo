67a0471b8fa5652ec90defaa65f1d25efcbed85e
Pushpak Bhagat <pbhagat@montran.com>
2022-05-01 15:16:39 +0530

    Setter Injection
    create variable FortuneService(Interface) ;
    set its implementation as <property name ="same as initialized in class" ref ="same as used in config file">
    create setter method

--- revision/com/pushpak/springdemo/coach/BaseBallCoach.java
+++ revision/com/pushpak/springdemo/coach/BaseBallCoach.java
@@ -1,7 +1,22 @@
 package com.pushpak.springdemo.coach;
 
+import com.pushpak.springdemo.fortuneServices.ForturneService;
+
 public class BaseBallCoach implements Coach
 {
+    public BaseBallCoach() {
+	System.out.println("inside noArg BB");
+    }
+
+    // setter injection
+    private ForturneService forturneService;
+
+    public void setForturneService(ForturneService forturneService)
+    {
+	System.out.println("setting forturneService");
+	this.forturneService = forturneService;
+    }
+    // setter injection end
 
     @Override
     public String getDailyWorkOut()
@@ -12,7 +27,7 @@ public class BaseBallCoach implements Coach
     @Override 
     public String getDailyForutune()
     {
-	return null;
+	return forturneService.getForturne();
     }
 
 }
